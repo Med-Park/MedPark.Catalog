4388153b5892dae069824447338020369a8d7dc4
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2020-03-11 14:17:45 +0200

    -added project

---
+++ src/Mappings/CatalogServiceMappings.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MedPark.Catalog.Domain;
+using MedPark.Catalog.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MedPark.Bookings.Mappings
+{
+    public class CatalogServiceMappings : Profile
+    {
+        public CatalogServiceMappings()
+        {
+            CreateMap<Product, ProductDetailDto>();
+            CreateMap<Category, CategoryDto>();
+            CreateMap<Category, CategoryDetailDto>();
+        }
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using MedPark.Catalog.Domain;
+using MedPark.Catalog.Dto;
+using MedPark.Catalog.Handlers.Catalog;
+using MedPark.Catalog.Queries;
+using MedPark.Common;
+using MedPark.Common.Handlers;
+using MedPark.Common.RabbitMq;
+using MedPark.Common.Redis;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace MedPark.Catalog
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public IContainer Container { get; private set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddRedis(Configuration);
+
+            //Add DBContext
+            services.AddDbContext<CatalogDBContext>(options => options.UseSqlServer(Configuration["Database:ConnectionString"]));
+
+            services.AddMvc(mvc => mvc.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterType<CatalogDBContext>().As<DbContext>().InstancePerLifetimeScope();
+            builder.RegisterAssemblyTypes(Assembly.GetEntryAssembly()).AsImplementedInterfaces();
+            builder.AddDispatchers();
+            builder.AddRabbitMq();
+            builder.AddRepository<Product>();
+            builder.AddRepository<Category>();
+            builder.AddRepository<ProductCatalog>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRabbitMq();
+
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
